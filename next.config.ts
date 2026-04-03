eed8ff3f2520ef8f4a4cc21356e554eae279090d
hoshikwyw <khaingwutyiwin1712@gmail.com>
2026-04-03 15:24:25 +0630

    Prebuild cleanup

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,5 @@
 import type { NextConfig } from "next";
-import withSerwistInit from "@serwist/next";
 
-const withSerwist = withSerwistInit({
-  swSrc: "app/sw.ts",
-  swDest: "public/sw.js",
-  disable: process.env.NODE_ENV === "development",
-});
+const nextConfig: NextConfig = {};
 
-const nextConfig: NextConfig = {
-  turbopack: {},
-};
-
-export default withSerwist(nextConfig);
+export default nextConfig;
